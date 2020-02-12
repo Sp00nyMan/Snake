0408c9a212aaba3f939cdf91859e31ee16191ef6
Sp00nyMan <mrstormproxdd@gmail.com>
2020-02-12 19:25:15 +0300

    Final

--- MainForm.cs
+++ MainForm.cs
@@ -1,25 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using Snake.Snake;
 
 namespace Snake
 {
     public partial class MainForm : Form
     {
+        public const uint fieldSize = 20;
+        private const int GridSquareSize = 30;
+        private const int updateDelay = GridSquareSize * 5;
+
+        private Snake.Snake snake = new Snake.Snake(fieldSize);
+        private Food food;
+        private Snake.Diractions? newDirection;
+
         public MainForm()
         {
             InitializeComponent();
+            ClientSize = new Size((int)fieldSize * GridSquareSize, (int)fieldSize * GridSquareSize); ;
         }
 
         private void MainPanel_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+        //    g.FillRectangle(new SolidBrush(Color.Black), new RectangleF(0, 0, MainPanel.Width, MainPanel.Height));
+            Brush brush = new SolidBrush(Color.Red);
+            g.FillRectangle(brush, new RectangleF(food.x * GridSquareSize, food.y * GridSquareSize, GridSquareSize, GridSquareSize));
+
+            brush = new SolidBrush(Color.Blue);
+            g.FillRectangle(brush, new RectangleF(snake.body[0].x * GridSquareSize, snake.body[0].y * GridSquareSize, GridSquareSize, GridSquareSize));
+
+            brush = new SolidBrush(Color.Green);
+            for (int i = 1; i < snake.body.Count; i++)
+            {
+                g.FillRectangle(brush, new RectangleF(snake.body[i].x * GridSquareSize, snake.body[i].y * GridSquareSize, GridSquareSize, GridSquareSize));
+            }
+        }
+
+        private void GameCycle()
+        {
+            if (snake.AteAllFood)
+                CreateFood();
+            bool gameFinished = !snake.Update(fieldSize, food,ref newDirection);
+            if (gameFinished)
+            {
+                Console.WriteLine(snake.Alive ? "WON" : "LOST");
+                GameTimer.Stop();
+            }
+        }
+
+        private void CreateFood()
+        {
+            Random random = new Random(System.DateTime.Now.Millisecond);
+
+            do
+                food = new Food(random.Next((int)fieldSize), random.Next((int)fieldSize));
+            while (snake == food);
+            snake.AteAllFood = false;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    newDirection = Diractions.down;
+                    break;
+                case Keys.Up:
+                    newDirection = Diractions.up;
+                    break;
+                case Keys.Left:
+                    newDirection = Diractions.left;
+                    break;
+                case Keys.Right:
+                    newDirection = Diractions.right;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GameCycle();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            GameTimer.Interval = updateDelay;
+            GameTimer.Start();
+            RepaintTimer.Interval = updateDelay / 10;
+            RepaintTimer.Start();
+        }
+
+        private void RepaintTimer_Tick(object sender, EventArgs e)
+        {
+            Refresh();
         }
     }
 }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -11,11 +11,11 @@ namespace Snake.Snake
         private const uint START_SNAKE_LENGHT = 4;
         private Diractions direction = Diractions.left;
 
-        private List<BodyPart> body = new List<BodyPart>((int) START_SNAKE_LENGHT);
-        private  bool Alive {get; set; } = true;
-        private  bool AteAllFood {get; set; } = true;
+        public List<BodyPart> body = new List<BodyPart>((int) START_SNAKE_LENGHT);
+        public  bool Alive {get; set; } = true;
+        public  bool AteAllFood {get; set; } = true;
 
-        public Snake(uint fielSize)
+        public Snake(uint fieldSize)
         {
             int xOffset = 0, yOffset = 0;
 
@@ -37,23 +37,27 @@ namespace Snake.Snake
 
             for (int i = 0; i < START_SNAKE_LENGHT; i++)
             {
-                int BPx = (int)(fielSize / 2 + xOffset * i - 1);
-                int BPy = (int)(fielSize / 2 + yOffset * i - 1);
+                int BPx = (int)(fieldSize / 2 + xOffset * i - 1);
+                int BPy = (int)(fieldSize / 2 + yOffset * i - 1);
                 body.Add(new BodyPart( BPx,BPy));
             }
         }
 
-        public bool Update(uint fieldSize, Food food, Diractions? newDir)
+        public bool Update(uint fieldSize, Food food, ref Diractions? newDir)
         {
             BodyPart headCopy = new BodyPart(body[0]);
 
-            Move(headCopy,newDir);
-            if(CollidedAWall(fieldSize, headCopy))
+            Move(ref headCopy,newDir);
+            if(CollidedAWall(fieldSize, headCopy) || CollidedItself(headCopy))
                 return Alive = false;
+            MoveSnake(headCopy);
+            Expand(headCopy, food);
+            if (body.Count == fieldSize * fieldSize)
+                return false;
             return Alive;
         }
 
-        private void Move(BodyPart head, Diractions? dir)
+        private void Move(ref BodyPart head, Diractions? dir)
         {
             int xOffset = 0, yOffset = 0;
             if (dir == null)
@@ -63,32 +67,31 @@ namespace Snake.Snake
                 case Diractions.up:
                     if(direction == Diractions.down) //Tried to move up while moving down
                         goto case Diractions.down;
-                    yOffset = 1;
+                    yOffset = -1;
                     direction = Diractions.up;
                     break;
                 case Diractions.down:
                     if (direction == Diractions.up) //Tried to move down while moving up
                         goto case Diractions.up;
-                    yOffset = -1;
+                    yOffset = 1;
                     direction = Diractions.down;
                     break;
                 case Diractions.left:
                     if (direction == Diractions.right) //Tried to move left while moving right
                         goto case Diractions.right;
-                    xOffset = 1;
+                    xOffset = -1;
                     direction = Diractions.left;
                     break;
                 case Diractions.right:
                     if (direction == Diractions.left) //Tried to move right while moving left
                         goto case Diractions.left;
-                    xOffset = -1;
+                    xOffset = 1;
                     direction = Diractions.right;
                     break;
             }
 
             head.move(xOffset, yOffset);
         }
-
         private bool CollidedAWall(uint fieldSize, BodyPart head)
         {
             if(head.x < 0 || head.x >= fieldSize || head.y < 0 || head.y >= fieldSize)
@@ -99,13 +102,42 @@ namespace Snake.Snake
         {
             for (int i = 1; i < body.Count; i++)
             {
-                
+                if (head == body[i])
+                    return true;
             }
             return false;
         }
+        private void MoveSnake(BodyPart newHead)
+        {
+            for (int i = this.body.Count - 1; i > 0; i--)
+                this.body[i].move(body[i - 1]);
+            this.body[0].move(newHead);
+        }
+        private void Expand(BodyPart head, Food food)
+        {
+            if (head == food)
+            {
+                AteAllFood = true;
+                body.Add(new BodyPart(body[body.Count - 1]));
+            }
+        }
+
+        public static bool operator ==(Snake snake, Food food)
+        {
+            foreach (var part in snake.body)
+            {
+                if (part == food)
+                    return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Snake snake, Food food)
+        {
+            return !(snake == food);
+        }
     }
 
-    struct BodyPart
+    public class BodyPart
     {
         public int x;
         public int y;
@@ -128,10 +160,33 @@ namespace Snake.Snake
             y += yOffset;
         }
 
-        public void move(ref BodyPart other)
+        public void move(BodyPart other)
         {
             x = other.x;
             y = other.y;
+        }
+
+        public static bool operator ==(BodyPart bp1, BodyPart bp2)
+        {
+            if (bp1.x == bp2.x && bp1.y == bp2.y)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(BodyPart bp1, BodyPart bp2)
+        {
+            return !(bp1 == bp2);
+        }
+
+        public static bool operator ==(BodyPart part, Food food)
+        {
+            if (part.x == food.x && part.y == food.y)
+                return true;
+            return false;
+        }
+        public static bool operator !=(BodyPart part, Food food)
+        {
+            return !(part == food);
         }
     }
 
@@ -145,6 +200,13 @@ namespace Snake.Snake
 
     public struct Food
     {
+        public int x;
+        public int y;
 
+        public Food(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
